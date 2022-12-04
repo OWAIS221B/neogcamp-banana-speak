64335be96b047f777e557c9eacb8b32fbeb839cc
Owais Ahmad <INFORM.OS221@GMAIL.COM>
2022-12-04 21:46:37 +0530

    done

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"\
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
 
 function getTranslationURL(text){
      return serverURL + "? " + "text=" + text
 }
 
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong with server: Try again after sometime");
+}
 
 function clickHandler() {
     
@@ -18,7 +24,11 @@ var inputText = txtInput.value; // taking input
    // calling server for processing
     fetch(getTranslationURL(inputText))
          .then( response => response.json())
-         .then(json => console.log(json.contents.translated))
+         .then(json => { 
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+         })
+         .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
