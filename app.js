11329c254476a8df325329d5d7ce78175b12f96f
Owais Ahmad <INFORM.OS221@GMAIL.COM>
2022-12-04 19:04:42 +0530

    getTranlationUrl

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){
+     return serverURL + "? " + "text=" + text
+}
+
+
 function clickHandler() {
     
-    outputDiv.innerText = "jsjsjsjs " + txtInput.value;
+    // outputDiv.innerText = "jsjsjsjs " + txtInput.value;
+var inputText = txtInput.value; // taking input
+
+   // calling server for processing
+    fetch(getTranslationURL(inputText))
+         .then( response => response.json())
+         .then(json => console.log(json.contents.translated))
 };
 
 btnTranslate.addEventListener("click", clickHandler)
