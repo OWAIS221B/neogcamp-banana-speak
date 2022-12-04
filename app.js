dd92db792f941641bae44ce5dc26d5699b4e6a34
Owais Ahmad <INFORM.OS221@GMAIL.COM>
2022-12-04 11:29:20 +0530

    input-output processing

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
 
 function clickHandler() {
     
-    console.log("clicked!");
-    console.log("input", txtInput.value);
-}
+    outputDiv.innerText = "jsjsjsjs " + txtInput.value;
+};
 
 btnTranslate.addEventListener("click", clickHandler)
 
